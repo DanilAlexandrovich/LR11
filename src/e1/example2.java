b33c2b7956b957a10a8a2b04e05f7e8e28997467
DanilAlexandrovich <vosmerka97@mail.ru>
2023-06-11 00:23:52 +0500

    Initial commit

--- src/e1/example2.java
+++ src/e1/example2.java
@@ -1,2 +1,10 @@
-package e1;public class example2 {
+package e1;
+
+public class example2 {
+    public static void m(int x){
+        if((2 * x + 1) < 20) m(2*x+1);
+        System.out.println("x=" + x);
+    }
+
+    public static void main(String[] args) { m(1); }
 }
--- src/e1/example5.java
+++ src/e1/example5.java
@@ -1,2 +1,13 @@
-package e1;public class example5 {
+package e1;
+
+public class example5 {
+    public static void main(String[] args) {
+        System.out.println(fact(8));
+    }
+
+    public static int fact(int n){
+        if(n==0) return 0;
+        else if(n==1) return 1;
+        else return fact(n-2) + fact(n-1);
+    }
 }
